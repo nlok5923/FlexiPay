b2e5019a3bbef6a2556b80641e17a370a07a4a95
Darshan Hande <darshanhande11@gmail.com>
2022-09-17 14:56:48 +0530

    added some missing points in vlad setup and updated nav

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import './Navbar.css'
 import { Menu, Button } from 'antd';
 import { ClusterOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 import { GiSplashyStream } from 'react-icons/gi'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div className='nav-div'>
         <Menu className='navbar' mode="horizontal" theme='dark'>
-            <div className='nav-logo-div'>
+            <div className='nav-logo-div' onClick={()=>navigate('/')}>
                 <GiSplashyStream className='nav-logo' />
                 <span className='logo-heading'>
                     FlexiPay
