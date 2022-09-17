86e1aa65ec2e7d86fe900b94999b490ef5785a07
Darshan Hande <darshanhande11@gmail.com>
2022-09-17 18:38:30 +0530

    added landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './LandingPage.css'
+import { RightOutlined } from '@ant-design/icons'
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>Landing Page</div>
+    <div className='lp-div'>
+      <div className='lp-intro-div'>
+        <h1 className='lp-intro-heading'>A Simple<br/>Pay-per-view<br/>for DAO- Events</h1>
+        <img src="/assets/images/man-laptop.png" alt="man-laptop.png" className='lp-intro-img' />
+      </div>
+      <div
+        className="lp-btn-div"
+        onClick={() => navigate('/events')}
+      >
+        <span className="lp-di-text">Get Started</span>
+        <RightOutlined className="lp-di-icon" />
+      </div>
+    </div>
   )
 }
 
