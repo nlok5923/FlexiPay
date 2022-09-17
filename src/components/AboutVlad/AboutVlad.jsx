00832f16721ba58543cae8f1c8e89461ea3e40a5
Darshan Hande <darshanhande11@gmail.com>
2022-09-17 14:56:48 +0530

    added some missing points in vlad setup and updated nav

--- src/components/AboutVlad/AboutVlad.jsx
+++ src/components/AboutVlad/AboutVlad.jsx
@@ -7,7 +7,7 @@ const AboutVlad = () => {
 
   return (
     <div className='av-div'>
-      <h1 className='av-heading'>Meet Vlad!</h1>
+      <h1 className='av-heading'>Meet Vlad !</h1>
       <div className='av-profile'>
         <img src="/assets/images/vlad.jpg" alt="vlad.jpg" className='av-avatar' />
         <p className='av-profile-text'>
@@ -15,13 +15,13 @@ const AboutVlad = () => {
         </p>
       </div>
       <div className='av-wv-div'>
-        <h2 className='av-heading'>Why Vlad?</h2>
+        <h2 className='av-heading'>Why Vlad ?</h2>
         <p className='av-reg-text av-wv-text'>
           Flexipay is based on the idea of paying for an event only for the amount of time you attend it, not more or not less. But, if there are a large number of participants attending the event, it would be really frustrating for the event organizers to regulate the participants. It would require more human force and wouldn’t be something productive to be done by humans. Here, Vlad comes to the event organizers' rescue. Vlad would only allow the participants who have registered and their stream running, to attend the event and would kick out the remaining participants. Vlad would also prevent spamming during the events.
         </p>
       </div>
       <div className='av-uv-div'>
-        <h2 className='av-heading'>How to set up and use Vlad?</h2>
+        <h2 className='av-heading'>How to set up and use Vlad ?</h2>
         <ul className='av-reg-text av-uv-ul'>
           <li>Copy the discord invite url for Vlad at the end of this page and paste it in your browser.</li>
           <li>Select the server to which you would like to add Vlad.</li>
@@ -30,6 +30,8 @@ const AboutVlad = () => {
               <code className='code-snippet'>{'!init eventInfo {eventId} {vc-name}'}</code>
           </li>
           <li>Replace <code className='code-snippet'>{'{eventId}'}</code> with the Event Id of your event and <code className='code-snippet'>{'{vc-name}'}</code> with the Voice Channel Name on which the event would be hosted.</li>
+          <li>Before the event starts make sure to add the bot to the Voice channel by entering the command <code className='code-snippet'>/join</code> on your #general channel.</li>
+          <li>After the event ends, you may remove the bot from the Voice channel by entering the command <code className='code-snippet'>/leave</code> on your #general channel.</li>
         </ul>
       </div>
       <div className='av-invite-div'>
